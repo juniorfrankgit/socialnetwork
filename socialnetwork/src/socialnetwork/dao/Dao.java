bfb03b5200ba35e9c693fa2b30b19a0f2a0aec53
Frank Junior <juniorngassam@gmail.com>
2016-04-05 22:57:22 -0700

    Login, logout

--- socialnetwork/src/socialnetwork/dao/Dao.java
+++ socialnetwork/src/socialnetwork/dao/Dao.java
@@ -7,9 +7,13 @@ package socialnetwork.dao;
 *
 */
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 
 import socialnetwork.model.User;
@@ -25,14 +29,17 @@ public enum Dao {
 		return users;
 	}
 	
-	public void add(String lastname, String firstname, String email,String password) {
+	public User add(String lastname, String firstname, String email,String password) {
 		synchronized(this) {
 			EntityManager em = EMFService.get().createEntityManager();
+			EntityTransaction tr = em.getTransaction();
 			User user = new User(new Long((long)(Math.random()*10000)),lastname,firstname,email,
 					password);
 			em.persist(user);
 			em.close();
+			return user;
 		}
+		
 	}
 	
 	public void remove(long id) {
@@ -44,4 +51,14 @@ public enum Dao {
 			em.close();
 		}
 	}
+	
+	public User getUser(String email){
+		EntityManager em = EMFService.get().createEntityManager();
+		Query q = em.createQuery("select m from User m where m.email = :email");
+		q.setParameter("email", email);
+		List<User> res = (List<User>)q.getResultList();
+		em.close();
+		return res.get(0);
+	}
+	
 }
